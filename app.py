6eb3391fe3a22f0dc2e9b842ee52d770602cfd2c
Luke Engelhard <luke.engelhard1@gmail.com>
2024-07-25 22:34:29 -0400

    fixing urls

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ s3_client = boto3.client('s3', region_name=S3_REGION)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'loginPage'
-CLOUDFRONT_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN', 'https://d18t3ps388njbv.cloudfront.net')
+CLOUDFRONT_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN', 'd18t3ps388njbv.cloudfront.net')
 
 # Set up logging
 if not app.debug:
