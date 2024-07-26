d7cf4ff0efb9d2f8dcf14cd1ab7bbab43d541c4d
Luke Engelhard <luke.engelhard1@gmail.com>
2024-07-25 20:51:31 -0400

    fixing lines

--- app.py
+++ app.py
@@ -485,21 +485,22 @@ def get_coordinatesFront():
     data = request.json
     x = data['xFront']
     y = data['yFront']
-    width = data['width']
-    height = data['height']
-    app.logger.info(f"Received coordinates for front: ({x}, {y}), width: {width}, height: {height}")
+    display_width = data['displayWidth']
+    display_height = data['displayHeight']
+    app.logger.info(f"Received coordinates for front: ({x}, {y}), display dimensions: {display_width}x{display_height}")
 
     imgFront = get_global_image(redis_client, 'imgFront')
     if imgFront is not None:
         img_copy = imgFront.copy()
-        processed_image = drawCross(img_copy, x, y, width, height)
+        actual_height, actual_width = img_copy.shape[:2]  # Get the actual dimensions of the image
+        x_scaled, y_scaled = scale_coordinates(x, y, display_width, display_height, actual_width, actual_height)
+        processed_image = drawCross(img_copy, x_scaled, y_scaled, actual_width, actual_height)
         set_saved_image(redis_client, "front", processed_image)
 
-        return jsonify({"message": "Coordinates received successfully."})
+        return jsonify({"message": "Coordinates received and processed successfully."})
     else:
         app.logger.error("imgFront not found in globalImages")
         return jsonify({"error": "imgFront not found"}), 400
-
 @app.route("/uploadFront", methods=['POST'])
 def uploadFront():
     redis_client = get_redis_client()
